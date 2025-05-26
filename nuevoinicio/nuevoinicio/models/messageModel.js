db6b0417af452b14259458dc8ac9571bf1a42699
Rubí <ramseymegan17@gmail.com>
2025-05-26 13:00:05 -0500

    FINAL

--- nuevoinicio/nuevoinicio/models/messageModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const messageSchema = new Schema({
-    username:String,
-    message:String
-})
-const Message = mongoose.model('Message', messageSchema)
-module.exports = Message
